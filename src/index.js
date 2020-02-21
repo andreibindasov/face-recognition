55fceede80bb2a16135d33386d4a84116a6c29e5
nexbook <60404451+nexbook@users.noreply.github.com>
2020-02-20 23:06:04 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import 'tachyons';
 
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
